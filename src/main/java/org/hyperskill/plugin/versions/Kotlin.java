2a690cbf3f30fed7a9dd03b378fe8bfdcbdd3830
Vladimir Turov <aaaaaa2493@yandex.ru>
2021-11-12 14:04:41 +0300

    Update Java version to 17

--- src/main/java/org/hyperskill/plugin/versions/Kotlin.java
+++ src/main/java/org/hyperskill/plugin/versions/Kotlin.java
@@ -3,7 +3,7 @@ package org.hyperskill.plugin.versions;
 public class Kotlin {
 
     public String version = "1.5.20";
-    public String javaVersion = "11";
+    public String javaVersion = "17";
 
     public Kotlin() {}
 }
