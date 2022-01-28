160ff7394d409efc9d7aacec7d6fce4d4a5471cb
RedJocker <64465262+RedJocker@users.noreply.github.com>
2022-01-27 21:48:25 -0300

    included gradle variables, updated version kotlin

--- src/main/java/org/hyperskill/plugin/versions/Android.java
+++ src/main/java/org/hyperskill/plugin/versions/Android.java
@@ -2,12 +2,16 @@ package org.hyperskill.plugin.versions;
 
 public class Android {
 
+    public String gradleVersion = "7.0.2";
+    public String androidGradleVersion = "7.0.2";
+    public String kotlinGradleVersion = "1.6.10";
+    public String kotlinVersion = "1.6.10";
+    
     public String robolectricVersion = "4.3.1";
-    public String kotlinVersion = "1.4.20";
-
-    public String compileSdkVersion = "29";
-    public String minSdkVersion = "20";
-    public String targetSdkVersion = "29";
+    
+    public Integer compileSdkVersion = 29;
+    public Integer minSdkVersion = 20;
+    public Integer targetSdkVersion = 29;
 
     public Android() {}
 
