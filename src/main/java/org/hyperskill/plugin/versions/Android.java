bdc3bb0893b8595733f3f10a1d4de92a09e368ac
RedJocker <64465262+RedJocker@users.noreply.github.com>
2022-02-09 15:45:06 -0300

    testing getAndroidGradleVersion(String gradleVersion)

--- src/main/java/org/hyperskill/plugin/versions/Android.java
+++ src/main/java/org/hyperskill/plugin/versions/Android.java
@@ -1,10 +1,10 @@
 package org.hyperskill.plugin.versions;
 
 public class Android {
-
+    
+    private String androidGradleVersion = "7.0.2";
+    private String androidGradleFallbackVersion = "4.0.2";
     public String gradleVersion = "7.0.2";
-    public String androidGradleVersion = "7.0.2";
-    public String androidGradleFallbackVersion = "4.0.2";
     public String kotlinGradleVersion = "1.6.10";
     public String kotlinVersion = "1.6.10";
     
@@ -15,5 +15,9 @@ public class Android {
     public Integer targetSdkVersion = 29;
 
     public Android() {}
+    
+    public String getAndroidGradleVersion(String gradleVersion) {
+        return gradleVersion.compareTo("7.0.2") < 0 ? androidGradleFallbackVersion : androidGradleVersion;
+    }
 
 }
