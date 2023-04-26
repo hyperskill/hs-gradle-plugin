ead90abb12fb13584c4bf48586a35f0f41409533
Sergey <polischuk0107@gmail.com>
2023-04-27 00:30:26 +0300

    Update Kotlin.java
    Upgrade Kotlin version to 1.8.20

--- src/main/java/org/hyperskill/plugin/versions/Kotlin.java
+++ src/main/java/org/hyperskill/plugin/versions/Kotlin.java
@@ -2,7 +2,7 @@ package org.hyperskill.plugin.versions;
 
 public class Kotlin {
 
-    public String version = "1.6.10";
+    public String version = "1.8.20";
     public String javaVersion = "17";
 
     public Kotlin() {}
