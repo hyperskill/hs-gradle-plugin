86bb96b808281f85b3a6f87abc1dfd21e96f6cbb
Sergey Polischuk <polischuk0107@gmail.com>
2024-10-31 18:25:39 +0200

    Downgrade Java to 17 for JBA plugin backward compatibility

--- src/main/java/org/hyperskill/plugin/versions/Kotlin.java
+++ src/main/java/org/hyperskill/plugin/versions/Kotlin.java
@@ -3,7 +3,7 @@ package org.hyperskill.plugin.versions;
 public class Kotlin {
 
     public String version = "2.0.0";
-    public String javaVersion = "21";
+    public String javaVersion = "17";
 
     public Kotlin() {}
 }
