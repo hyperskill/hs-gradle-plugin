0c02d4e81525808c2397fee7760946ffc0d1a6ca
Sergey <polischuk0107@gmail.com>
2024-11-21 07:12:32 +0200

    updated Android dependencies version (#12)

--- src/main/java/org/hyperskill/plugin/versions/Android.java
+++ src/main/java/org/hyperskill/plugin/versions/Android.java
@@ -2,24 +2,24 @@ package org.hyperskill.plugin.versions;
 
 public class Android {
 
-    private String androidGradleVersion = "7.4.0";
-    public String gradleVersion = "7.5";
-    public String kotlinGradleVersion = "1.6.10";
-    public String kotlinVersion = "1.6.10";
+    private String androidGradleVersion = "8.7.2";
+    public String gradleVersion = "8.9";
+    public String kotlinGradleVersion = "1.9.22";
+    public String kotlinVersion = "1.8.20";
 
-    public String robolectricVersion = "4.7.3";
+    public String robolectricVersion = "4.14";
 
-    public Integer compileSdkVersion = 29;
-    public Integer minSdkVersion = 20;
-    public Integer targetSdkVersion = 29;
+    public Integer compileSdkVersion = 34;
+    public Integer minSdkVersion = 21;
+    public Integer targetSdkVersion = 34;
 
     public Lib lib = new Lib();
 
     public Android() {}
 
     public String getAndroidGradleVersion(String gradleVersion) {
-        final String androidGradleFallbackVersion = "4.0.2";
-        final String minimumGradleVersionNotRequiringFallback = "7.0.2";
+        final String androidGradleFallbackVersion = "7.0.2";
+        final String minimumGradleVersionNotRequiringFallback = "8.0.0";
 
         if (gradleVersion.compareTo(minimumGradleVersionNotRequiringFallback) < 0) {
             return androidGradleFallbackVersion;
@@ -30,10 +30,10 @@ public class Android {
 
     public static class Lib {
         public String junit = "4.13.2";
-        public String appCompat = "1.3.1";
-        public String coreKtx = "1.6.0";
-        public String constraintLayout = "2.1.3";
-        public String material = "1.4.0";
+        public String appCompat = "1.7.0";
+        public String coreKtx = "1.15.0";
+        public String constraintLayout = "2.0.4";
+        public String material = "1.11.0";
 
         public Lib() {}
     }
