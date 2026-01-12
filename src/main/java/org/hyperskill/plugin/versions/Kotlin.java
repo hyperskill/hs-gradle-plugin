2bb67264c24e8a1e98a21e8d3fbf51bf8ca23a11
Igor Kirillov <igor.kirillov@hyperskill.org>
2026-01-12 12:32:18 +0100

    Downgrade Kotlin ver

--- src/main/java/org/hyperskill/plugin/versions/Kotlin.java
+++ src/main/java/org/hyperskill/plugin/versions/Kotlin.java
@@ -2,7 +2,7 @@ package org.hyperskill.plugin.versions;
 
 public class Kotlin {
 
-    public String version = "2.3.0";
+    public String version = "2.1.0";
     public String javaVersion = "23";
 
     public Kotlin() {}
